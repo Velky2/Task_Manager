99fe85fa970c687e443561a36cca4d35b10f1a73
nicejammer <158223817+nicejammer@users.noreply.github.com>
2025-07-03 03:18:53 -0300

    add more markdown functions to terminal_utils.py

--- terminal_utils.py
+++ terminal_utils.py
@@ -8,4 +8,28 @@ def clear_screen() -> None:
 
 
 def bold(text: str) -> str:
-    return "\033[1m" + text + "\033[0m"
+    return "\033[1m" + text + "\033[22m"
+
+
+def italic(text: str) -> str:
+    return "\033[3m" + text + "\033[23m"
+
+
+def underline(text: str) -> str:
+    return "\033[4m" + text + "\033[24m"
+
+
+def blinking(text: str) -> str:
+    return "\033[5m" + text + "\033[25m"
+
+
+def inverse(text: str) -> str:
+    return "\033[7m" + text + "\033[27m"
+
+
+def hidden(text: str) -> str:
+    return "\033[8m" + text + "\033[28m"
+
+
+def strikethrough(text: str) -> str:
+    return "\033[9m" + text + "\033[29m"
